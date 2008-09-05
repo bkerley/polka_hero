c8d17d6e73328551cbe1b4ebc68ce9c45ff1370e
Bryce Kerley <bryce@worldmedia.net>
2008-09-05 10:47:36 -0400

    refactored to support image replacement, etc.

--- polka_hero.js
+++ polka_hero.js
@@ -4,9 +4,9 @@ Array.prototype.cdr = function() {
 Array.prototype.car = function() {
 	return this[0];
 }
-var ph_animation_options = {duration: 0.4};
-var shown_image = '/images/colintalk-23x32.png';
-var hidden_image= '/images/colin-23x32.png';
+var ph_animation_options = {duration: 0.3};
+var shown_image = 'images/colintalk-23x32.png';
+var hidden_image= 'images/colin-23x32.png';
 var PolkaHero = Class.create({
 	initialize: function(element) {
 		this.element = element;
@@ -15,40 +15,37 @@ var PolkaHero = Class.create({
 			sb.para = sb.select('div.sbody')[0];
 			sb.sel = sb.select('h1')[0];
 			sb.img = sb.select('h1 img')[0];
+			
+			sb.hidden = true;
+			sb.ph_hide = function() {
+				if (sb.hidden) return;
+				Effect.BlindUp(sb.para, ph_animation_options);
+				sb.hidden = true;
+				sb.img.src = hidden_image;
+			};
+			sb.ph_show = function() {
+				if (!sb.hidden) return;
+				Effect.BlindDown(sb.para, ph_animation_options);
+				sb.hidden = false;
+				sb.img.src = shown_image;
+			};
 		})
-		this.paragraphs.each(function(pp) {
-			pp.hidden = true;
-			pp.image = pp.select("h1 img")[0];
-			pp.ph_hide = function() {
-				if (pp.hidden) return;
-				Effect.BlindUp(pp, ph_animation_options);
-				pp.hidden = true;
-				pp.image.src = hidden_image;
-			};
-			pp.ph_show = function() {
-				if (!pp.hidden) return;
-				Effect.BlindDown(pp, ph_animation_options);
-				pp.hidden = false;
-				pp.image.src = shown_image;
-			};
-			new Effect.Highlight(pp);
-		});
 		
 		this.first_cycle();
 		this.bind_events();
 	},
 	first_cycle: function() {
-		var paras = this.paragraphs;
-		var count = paras.length;
+		var sbs = this.squeezeboxes;
+		var count = sbs.length;
 		var i = 0;
 		
-		paras.car().ph_show();
+		sbs.car().ph_show();
 		
 		new PeriodicalExecuter(function(pe) {
 			var hide_me = i;
 			var show_me = (i+1) % count;
-			paras[hide_me].ph_hide();
-			paras[show_me].ph_show();
+			sbs[hide_me].ph_hide();
+			sbs[show_me].ph_show();
 			i++;
 			if (i == count) {
 				pe.stop();
@@ -56,15 +53,12 @@ var PolkaHero = Class.create({
 		}, 2);
 	},
 	bind_events: function() {
-		var seles = this.selectors;
-		var paras = this.paragraphs;
+		var sbs = this.squeezeboxes;
 		
-		for(var i = 0; i < seles.length; i++) {
-			var clicking = seles[i];
-			clicking._hides_these = paras.without(paras[i]);
-			clicking._shows_this = paras[i];
-			clicking._image_on = clicking.select('img')[0];
-			clicking._image_off = seles.without(clicking).select
+		for(var i = 0; i < sbs.length; i++) {
+			var clicking = sbs[i].sel;
+			clicking._hides_these = sbs.without(sbs[i]);
+			clicking._shows_this = sbs[i];
 			
 			var like_this = function(event) {
 				var e = event.element();
